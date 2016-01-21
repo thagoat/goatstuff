bf159a88379a46b44f4844be39f34077523c6f88
thagoat <noreply@thagoat.com>
2016-01-20 23:33:11 -0500

    Update new_server.sh

--- new_server.sh
+++ new_server.sh
@@ -9,7 +9,7 @@ apt-get update;
 apt-get install -y nano apache2 mysql-server php5 python-pip;
 
 ## Sudo
-echo "thagoat ALL=(ALL:ALL) ALL' >> /etc/sudoers;
+echo "thagoat ALL=(ALL:ALL) ALL" >> /etc/sudoers;
 echo "alias upd='sudo apt-get update; sudo apt-get -y upgrade; sudo apt-get -y autoremove'" >> /home/thagoat/.bash_aliases;
 echo "alias sagi='sudo apt-get install -y'" >> /home/thagoat/.bash_aliases;
 source /home/thagoat/.bashrc;
